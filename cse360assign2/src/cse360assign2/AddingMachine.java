7ed11bddb5ea409125df3ce1ebec6fa3f8eab10c
meeatingchips <meeatingchips@alexs-mbp.3521.dhcp.asu.edu>
2019-10-08 14:36:57 -0700

    AddingMachine.java has Javadoc commenting style on empty methods.

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,47 @@
+/****************************************************************
+ * Sergio Barraza
+ * ASU Hybrid CSE360-70642
+ * Assignment 2
+ * 
+ * Description: AddingMachine class will allow a user to enter
+ * numbers and operations to either add or subtract. The class
+ * will also track every number and operation used consecutively
+ * in a string.
+ ****************************************************************/
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	// Default Constructor.
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	// Returns the total.
+	public int getTotal () {
+		return 0;
+	}
+	
+	// Adds the values.
+	public void add (int value) {
+		
+	}
+	
+	// Subtracts the values.
+	public void subtract (int value) {
+		
+	}
+	
+	// Returns a string of the user input (numbers and operations).
+	public String toString () {
+		return "";
+	}
+	
+	// Clears all to default.
+	public void clear() {
+	
+	}
+}
